f9c7ac0c9436aced8032b4f06cf5b59ce8dd4b74
Strixis <nivem-strixis@mail.ru>
2020-11-25 23:47:23 +0300

    Сделана сборка простейшего React-кода

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-import { User } from './user.js'
+import React, { createElement } from 'react';
+import ReactDom from 'react-dom';
 
-const user = new User();
-console.log(user.sayHi());
+// Оставлю это пока здесь.
+/* const element = createElement(
+    'h1',
+    { className: 'react-hello' },
+    'React is working!',
+); */
+/* const element = <h1 class="react-hello">React is working with jsx!</h1>; */
+
+const messages = ['Hi!', 'How are you?', "I'm fine, thanks."];
+
+const Message = (props) => <div>{ props.text }</div>;
+const MessageList = (props) => {
+    return props.messages.map((message) => <Message text={ message } />)
+};
+
+ReactDom.render(
+    <MessageList messages={ messages } />,
+    document.getElementById('root'),
+);
