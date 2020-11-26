a8cca5c86d2f9a11486f6baa0e97c182e20f1708
Strixis <nivem-strixis@mail.ru>
2020-11-26 17:12:36 +0300

    Небольшие правки кода

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import React, { createElement } from 'react';
 import ReactDom from 'react-dom';
 
-// Оставлю это пока здесь.
-/* const element = createElement(
-    'h1',
-    { className: 'react-hello' },
-    'React is working!',
-); */
-/* const element = <h1 class="react-hello">React is working with jsx!</h1>; */
-
 const messages = ['Hi!', 'How are you?', "I'm fine, thanks."];
 
 const App = (props) => {
     return (
-    <div>
-        <MessageList messages={ messages } />
-        <SendMessageButton />
-    </div>
+        <div>
+            <MessageList messages={ messages } />
+            <SendMessageButton />
+        </div>
     )
 };
 
@@ -25,11 +17,7 @@ const MessageList = (props) => {
 };
 const Message = (props) => <div>{ props.text }</div>;
 
-const SendMessageButton = (props) => {
-    return <button
-    onClick={ sendNewMessage }
-    >Click Me!</button>
-};
+const SendMessageButton = (props) => <button onClick={ sendNewMessage }>Click Me!</button>;
 
 function sendNewMessage() {
     messages.push("I'm a new message!");
