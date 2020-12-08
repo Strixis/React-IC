c1e3543dfdabadd22448764158e7b47c1d67cae8
Strixis <nivem-strixis@mail.ru>
2020-12-08 13:36:28 +0300

    Логика перенесена в Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,20 +3,110 @@ import './Layout.less';
 import React, { Component } from 'react';
 import { ChatList } from 'components/ChatList';
 import { Header } from 'components/Header';
-import { Messanger } from '../Messanger/Messanger';
+import { Messanger } from 'components/Messanger';
+import { Bot } from 'components/Bot';
 
 class Layout extends Component {
-render() {
-  return (
-    <div>
-      <Header />
-      <div className="chat-space">
-        <ChatList />
-        <Messanger chatId={ this.props.match.params.id } />
+  state = {
+    chats: {
+      '1': {
+        id: 1,
+        name: 'Chat 1',
+        messages: [
+          {
+            author: 'Bot',
+            text: 'This is chat 1.',
+          }
+        ],
+      },
+      '2': {
+        id: 2,
+        name: 'Chat 2',
+        messages: [
+          {
+            author: 'Bot',
+            text: 'This is chat 2.',
+          }
+        ],
+      },
+      '3': {
+        id: 3,
+        name: 'Chat 3',
+        messages: [
+          {
+            author: 'Bot',
+            text: 'This is chat 3.',
+          }
+        ],
+      },
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.messages) {
+      const { chats } = this.state;
+      const { match } = this.props;
+
+      const chat = chats[match.params.id];
+
+      const messages = this.messages;
+      const message = messages[messages.length - 1];
+
+      if (message.author !== 'Bot') {
+        setTimeout(() => {
+          chat.messages = messages.concat(Bot.getAnswer(message));
+
+          this.setState({
+            chats: {
+              ...this.state.chats,
+              [match.params.id]: chat,
+            }
+          })
+        }, 1000);
+      }
+    }
+  }
+
+  handleMessageSend = (message) => {
+    const { chats } = this.state;
+    const { match } = this.props;
+
+    const chat = chats[match.params.id];
+    const messages = this.messages.concat([message]);
+    chat.messages = messages;
+
+    this.setState({
+      chats: {
+        ...this.state.chats,
+        [match.params.id]: chat,
+      }
+    })
+  }
+
+  get messages() {
+    const { chats } = this.state;
+    const { match } = this.props;
+    
+    let messages = null;
+
+    if (match && chats[match.params.id]) {
+      messages = chats[match.params.id].messages;
+    }
+
+    return messages;
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="chat-space">
+          <ChatList />
+          <Messanger onSend={ this.handleMessageSend } messages={ this.messages } />
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 export { Layout };
--- src/components/Messanger/Messanger.jsx
+++ src/components/Messanger/Messanger.jsx
@@ -1,107 +1,25 @@
 import './Messanger.less';
 
 import React, { Component } from 'react';
+import PropType from 'prop-types';
 
 import { MessagesList } from 'components/MessageList';
 import { MessageField } from 'components/MessageField';
-import { Bot } from 'components/Bot';
 
 class Messanger extends Component {
-  state = {
-    chats: {
-      '1': {
-        id: 1,
-        name: 'Chat 1',
-        messages: [
-          {
-            author: 'Bot',
-            text: 'This is chat 1.',
-          }
-        ],
-      },
-      '2': {
-        id: 2,
-        name: 'Chat 2',
-        messages: [
-          {
-            author: 'Bot',
-            text: 'This is chat 2.',
-          }
-        ],
-      },
-      '3': {
-        id: 3,
-        name: 'Chat 3',
-        messages: [
-          {
-            author: 'Bot',
-            text: 'This is chat 3.',
-          }
-        ],
-      },
-    }
-  }
-
-  componentDidUpdate() {
-    if (this.messages) {
-      const { chats } = this.state;
-      const { chatId } = this.props;
-
-      const chat = chats[chatId];
-
-      const messages = this.messages;
-      const message = messages[messages.length - 1];
-
-      if (message.author !== 'Bot') {
-        setTimeout(() => {
-          chat.messages = messages.concat(Bot.getAnswer(message));
-          console.log(chat.messages);
-
-          this.setState({
-            chats: {
-              ...this.state.chats,
-              [chatId]: chat,
-            }
-          })
-        }, 1000);
-      }
-    }
-  }
-
-  handleMessageSend = (message) => {
-    const { chats } = this.state;
-    const { chatId } = this.props;
-
-    const chat = chats[chatId];
-    const messages = this.messages.concat([message]);
-    chat.messages = messages;
-
-    this.setState({
-      chats: {
-        ...this.state.chats,
-        [chatId]: chat,
-      }
-    })
-  }
-
-  get messages() {
-    const { chats } = this.state;
-    const { chatId } = this.props;
-    
-    let messages = null;
-
-    if (chatId && chats[chatId]) {
-      messages = chats[chatId].messages;
-    }
-
-    return messages;
+  static propTypes = {
+    onSend: PropType.func,
+    messages: PropType.array,
   }
 
   render() {
+    const { messages } = this.props;
+    const { onSend } = this.props;
+
     return (
       <div className="messanger">
-        { this.messages ? <MessagesList messages={ this.messages }/> : <p>Выберите чат.</p> }
-        { this.messages && <MessageField onSend={ this.handleMessageSend } /> }
+        { messages ? <MessagesList messages={ messages }/> : <p>Выберите чат.</p> }
+        { messages && <MessageField onSend={ onSend } /> }
       </div>
     )
   }
